6b799e9c0ee0b9c105960e8061b824c602440cdf
VavilonBase <VavilonBase@mail.ru>
2022-09-05 10:13:57 +0300

    Add selection sort with linked list

--- Sorting By Choice/SortingbyChoice/src/Main.java
+++ Sorting By Choice/SortingbyChoice/src/Main.java
@@ -15,6 +15,26 @@ public class Main {
         } else {
             System.out.println("List is empty");
         }
+
+        System.out.println("");
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(3);
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+        myLinkedList.add(4);
+        myLinkedList.add(7);
+        myLinkedList.add(5);
+        myLinkedList.add(6);
+        myLinkedList.add(9);
+        myLinkedList.add(10);
+        myLinkedList.add(8);
+
+        myLinkedList.print();
+        System.out.println("");
+
+        MyLinkedList sortedLinkedList = myLinkedList.selectionSort();
+        sortedLinkedList.print();
     }
 
     public static int[] selectionSort(List<Integer> arr) {
